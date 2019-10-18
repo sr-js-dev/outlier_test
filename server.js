bb04e8e8010176ba4f0a549766f762790cc3fc98
madanodyblum <gorayblum@gmail.com>
2019-10-18 20:56:39 +0100

    user-agent

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url === '/base64') return respondBase64(req, res)
+  if (req.url === '/user') return respondUser(req, res)
 
   res.end()
 })
@@ -14,6 +15,9 @@ function respondHello (req, res) {
 }
 function respondBase64 (req, res) {
   res.end(JSON.stringify({ msg: 'base64' }))
+}
+function respondUser (req, res) {
+  res.end(JSON.stringify({ msg: 'user-agent' }))
 }
 
 server.listen(PORT)
