058b205a25979bd6ec7cdb6285d268ce88b8d8d3
madanodyblum <gorayblum@gmail.com>
2019-10-18 20:55:21 +0100

    base64 endpoint

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/base64') return respondBase64(req, res)
 
   res.end()
 })
 
 function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
+  res.end(JSON.stringify({ msg: 'hello1' }))
+}
+function respondBase64 (req, res) {
+  res.end(JSON.stringify({ msg: 'base64' }))
 }
 
 server.listen(PORT)
